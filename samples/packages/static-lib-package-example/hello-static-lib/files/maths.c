8b390adc6bb8dc15a03152490da807a4b1a02f0c
pksh-23514 <prabhat23514k@gmail.com>
2025-10-12 11:27:45 +0530

    C File Built Using Recipe

---
+++ samples/packages/static-lib-package-example/hello-static-lib/files/maths.c
@@ -0,0 +1,11 @@
+#include "static_lib.h"
+
+int add (int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+int sub (int num1, int num2)
+{
+	return ((num1 > num2) ? (num1 - num2) : (num2 - num1));
+}
